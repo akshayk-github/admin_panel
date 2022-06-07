73b8eef9ad321d68f7ccdc1bd7ef4a0fecd1fa53
unknown <akshaykumbhani1919@gmail.com>
2022-06-07 09:54:26 +0530

    edit - delete data

--- src/container/Medicine/Medicine.js
+++ src/container/Medicine/Medicine.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -6,14 +6,22 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { DataGrid } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { IconButton } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
 
 function Medicine(props) {
 
     const [open, setOpen] = React.useState(false);
-    const [name, setName] = React.useState("");
-    const [price, setPrice] = React.useState("");
-    const [quantity, setQuantity] = React.useState("");
-    const [expiry, setExpiry] = React.useState("");
+    const [deleteopen, setDeleteopen] = useState(false);
+    const [editopen, setEditopen] = useState(false);
+    const [name, setName] = useState("");
+    const [did, setDid] = useState("");
+    const [price, setPrice] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [expiry, setExpiry] = useState("");
+    const [showData, setShowData] = useState([])
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -21,10 +29,36 @@ function Medicine(props) {
 
     const handleClose = () => {
         setOpen(false);
+        setDeleteopen(false);
+        setEditopen(false);
     };
+
+    const handleClickdelete = (id) => {
+        setDid(id)
+        setDeleteopen(true);
+    };
+
+    const handleClickopen = (id) => {
+        setEditopen(true);
+        console.log(id);
+        // EditData(id);
+
+    };
+
+    const handleDelete = () => {
+
+        let getDataItem = JSON.parse(localStorage.getItem("medicine"));
+
+        let GFilter = getDataItem.filter((g, i) => g.id !== did)
+
+        localStorage.setItem("employee", JSON.stringify(GFilter))
+        getdata();
+        deleteopen(false);
+    }
 
     const handleSubmit = () => {
         let data = {
+            id: Math.floor(Math.random() * 1000),
             name,
             price,
             quantity,
@@ -38,16 +72,53 @@ function Medicine(props) {
         if (medicineData == null) {
             localStorage.setItem('medicine', JSON.stringify([data]));
             console.log(medicineArray);
-        }else {
+        } else {
             medicineData.push(data)
             localStorage.setItem('medicine', JSON.stringify(medicineData));
         }
         handleClose();
+        getdata();
 
         // console.log(data);
     }
 
-    
+    const getdata = () => {
+        const getDataItem = JSON.parse(localStorage.getItem("medicine"));
+
+        if (getDataItem !== null) {
+            setShowData(getDataItem);
+        }
+    }
+
+    useEffect(() => {
+        getdata();
+    },
+        []
+    )
+
+
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'name', headerName: 'Medicine name', width: 130 },
+        { field: 'price', headerName: 'Price', width: 130 },
+        { field: 'quantity', headerName: 'Quantity', width: 130 },
+        { field: 'expiry', headerName: 'Expiry', width: 130 },
+        {
+            field: 'action', headerName: 'Action', width: 130,
+            renderCell: (params) => {
+                return (
+                    <>
+                        <IconButton onClick={() => handleClickdelete(params.id)} aria-label="delete">
+                            <DeleteIcon />
+                        </IconButton>
+                        <IconButton onClick={() => handleClickOpen(params.id)} aria-label="edit">
+                            <EditIcon />
+                        </IconButton>
+                    </>
+                )
+            }
+        }
+    ];
 
     return (
         <div>
@@ -55,11 +126,21 @@ function Medicine(props) {
             <h1>Medicine</h1>
 
             <div>
+                <Dialog open={deleteopen} onClose={handleClose}>
+                    <DialogTitle>Are you sure Delete ?</DialogTitle>
+                    <DialogActions>
+                        <Button onClick={handleClose}>No</Button>
+                        <Button onClick={handleDelete}>Yes</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+
+            <div>
                 <Button variant="outlined" onClick={handleClickOpen}>
                     Medicine
                 </Button>
                 <Dialog open={open} onClose={handleClose}>
-                    <DialogTitle>Subscribe</DialogTitle>
+                    <DialogTitle>Medicine Data</DialogTitle>
                     <DialogContent>
                         <TextField
                             autoFocus
@@ -112,6 +193,17 @@ function Medicine(props) {
                     </DialogActions>
                 </Dialog>
             </div>
+
+            <div style={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    rows={showData}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                />
+            </div>
+
         </div>
     );
 }
